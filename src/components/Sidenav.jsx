37245a8e71121ed097b515b804c08c637c1e026c
tajaa <tajatheprince@gmail.com>
2023-06-12 11:55:37 -0700

    third commit: added contact section and form

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -25,6 +25,7 @@ const Sidenav = () => {
       {nav ? (
         <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-50">
           <a
+            onClick={handleNav}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 "
           >
@@ -32,6 +33,7 @@ const Sidenav = () => {
             <span className="pl-4">Home</span>
           </a>
           <a
+            onClick={handleNav}
             href="#work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 "
           >
@@ -39,6 +41,7 @@ const Sidenav = () => {
             <span className="pl-4">Work</span>
           </a>
           <a
+            onClick={handleNav}
             href="#projects"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 "
           >
@@ -46,6 +49,7 @@ const Sidenav = () => {
             <span className="pl-4">Projects</span>
           </a>
           <a
+            onClick={handleNav}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 "
           >
@@ -53,6 +57,7 @@ const Sidenav = () => {
             <span className="pl-4">Resume</span>
           </a>
           <a
+            onClick={handleNav}
             href="#contact"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 "
           >
